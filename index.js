e22fa961d7e8af5c087b6c7be61d267c9e0938b7
jacinta mwangi <jacinta.mwangi@student@moringaschool.com>
2022-12-03 19:56:17 +0300

    Solved Arithmetics Lab

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
-// Write your code here
+// Write your code
+var num1;
+var num2;
+num1=10;
+num2=6.2;
+var multiply=num1*num2;
+
+console.log(multiply)
+
+var num3=68;
+var num4=8
+var mod = num3%num4
+
+console.log(mod)
+
+
+let random=Math.floor(Math.random() * 10);
+random=1
+console.log(random)
+
+const max=Math.max(3, 19.9, 15, 20);
+
+console.log(max)
+
+
